5130385c2461de095f6496962144afaf451d7cc5
Sumit Pawar <sumitppawar@gmail.com>
2018-07-18 22:27:31 +0530

    Added email spam classification example

--- build.sbt
+++ build.sbt
@@ -8,6 +8,5 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.3.1",
   "org.apache.spark" %% "spark-mllib" % "2.3.1",
   "org.apache.spark" %% "spark-sql" % "2.3.1",
-  "org.apache.spark" %% "spark-graphx" % "2.3.1",
-  "com.typesafe.play" %% "play-json" % "2.6.7"
+  "org.apache.spark" %% "spark-graphx" % "2.3.1"
 )
--- src/main/scala/learn/apache/spark/DriverProgram.scala
+++ src/main/scala/learn/apache/spark/DriverProgram.scala
@@ -1,15 +1,45 @@
 package learn.apache.spark
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.ml.feature.HashingTF
-
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.feature.HashingTF
+import org.apache.spark.mllib.regression.LabeledPoint
 object DriverProgram {
   def main(args: Array[String]): Unit = {
+
     val sc = createSparkContext()
-    sc.textFile("build.sbt")
-    print(sc.textFile("build.sbt").count)
-    sc.stop
+    //Load Data
+    val spamRdd = sc.textFile("docs/spam.txt")
+    val nonSpamRdd = sc.textFile("docs/nonspam.txt")
+
+    val tf = new HashingTF()
+    //Feature Extraction
+    val spamFeature = spamRdd.map(line => tf.transform(line.toUpperCase.split(" ")))
+    val nonSpamFeature = nonSpamRdd.map(line => tf.transform(line.toUpperCase.split(" ")))
+
+    //Label Data
+    val negativeExamples = spamFeature.map(feature => LabeledPoint(1, feature))
+    val positiveExamples = nonSpamFeature.map(feature => LabeledPoint(0, feature))
+
+    //Model
+    val trainingData = positiveExamples union negativeExamples
+    trainingData.cache()
+    val model = new LogisticRegressionWithLBFGS().run(trainingData)
+
+    val negativeTest = tf.transform(
+      "Additional income Click here link Free cell phone"
+        .toUpperCase
+        .split(" ")
+    )
+
+    val positveTest = tf.transform(
+      "Hey Dad, How are you ? Plaese send some extra money for this month"
+        .toUpperCase
+        .split(" ")
+    )
+
+    println(s"Predication for positive test example: ${model.predict(positveTest)}")
+    println(s"Predication for negative test example: ${model.predict(negativeTest)}")
   }
 
   private def createSparkContext():SparkContext = new SparkContext(createSparkConf())
